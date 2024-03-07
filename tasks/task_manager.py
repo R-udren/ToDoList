744dd0f07c5b5f09530b9197b5d73419ef1ac816
Robert <54269637+R-udren@users.noreply.github.com>
2024-03-07 13:44:11 +0000

    Add TaskManager class with basic methods

---
+++ tasks/task_manager.py
@@ -0,0 +1,15 @@
+class TaskManager:
+    def create_task(self):
+        pass
+
+    def update_task(self):
+        pass
+
+    def delete_task(self):
+        pass
+
+    def list_tasks(self):
+        pass
+
+    def exit(self):
+        pass
