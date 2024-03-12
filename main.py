9d0b389c519f7b66fc2ef2c37ad6c53cef95ed0e
Robert <54269637+R-udren@users.noreply.github.com>
2024-03-12 09:34:08 +0000

    Refactor menu function and add command handling in TaskManager

--- main.py
+++ main.py
@@ -2,31 +2,37 @@ from rich.prompt import Prompt
 from rich.console import Console
 from rich.table import Table
 
+from tasks.task_manager import TaskManager
+
 console = Console()
 
-options = [
-        "Create a task",
-        "Update a task",
-        "Delete a task",
-        "List all tasks",
-        "Exit"
-    ]
 
 def create_options_table():
     table = Table(title="Options", title_style="bold blue")
     table.add_column("Nr", style="cyan", justify="center")
     table.add_column("Description", style="magenta")
 
-    for i, option in enumerate(options, 1):
+    for i, option in enumerate(TaskManager.commands, 1):
         table.add_row(str(i), option)
 
     return table
 
-def menu():
+def options_menu():
     console.print(create_options_table())
-    option = Prompt.ask("Select an option", choices=[str(i) for i in range(1, len(options) + 1)])
+    option = int(Prompt.ask("Select an option", choices=[str(i) for i in range(1, len(TaskManager.commands) + 1)]))
 
-    return int(option)
+    task_manager = TaskManager()
+    task_manager.command(option)
+
+def menu():
+    while True:
+        try:
+            options_menu()
+        except KeyboardInterrupt:
+            console.print("\n[bold yellow]Goodbye![/bold yellow]")
+            break
+
+
 
 if __name__ == "__main__":
     menu()
--- tasks/task_manager.py
+++ tasks/task_manager.py
@@ -1,15 +1,40 @@
+from tasks.task import Task
+
 class TaskManager:
-    def create_task(self):
+    commands = [
+        "Create a task",
+        "Update a task",
+        "Delete a task",
+        "List all tasks",
+        "Exit"
+    ]
+
+    def command(self, option: int):
+        match option:
+            case 1:
+                task = Task()
+                self.create_task(task)
+            case 2:
+                self.update_task()
+            case 3:
+                self.delete_task()
+            case 4:
+                self.list_tasks()
+            case 5:
+                self.exit()
+
+    def create_task(self, task: Task):
         pass
 
-    def update_task(self):
+    def update_task(self, old_task: Task, new_task: Task):
         pass
 
-    def delete_task(self):
+    def delete_task(self, task_to_delete: Task):
         pass
 
-    def list_tasks(self):
+    def list_tasks(self, tasks: list[Task], sort_by=None, filter_by=None):
         pass
 
-    def exit(self):
-        pass
+    @staticmethod
+    def exit():
+        raise KeyboardInterrupt
