920d96a7e0f2207edf11caab3653b95730273f49
MarksGB <131261270+22DP3MBruv@users.noreply.github.com>
2024-03-14 13:01:15 +0000

    Made several task actions that reflect onto the database

--- tasks/task_manager.py
+++ tasks/task_manager.py
@@ -18,18 +18,19 @@ class TaskManager:
     def __init__(self):
         self.db = DatabaseManager(DB_NAME)
         self.db.create_table("tasks", ["description TEXT", "complete INTEGER", "due_date TEXT", "priority TEXT", "create_date TEXT"])
-        self.tasks = self.db.read_records("tasks")
+        self.records = self.db.read_records("tasks")
+        self.tasks = [Task(*record) for record in self.records]
 
 
     def command(self, option: int):
         match option:
             case 1:
                 task = ui.create_task()
-                self.create_task(task)
+                self.create_task(ui.create_task())
             case 2:
                 self.update_task(ui.create_task(), self.tasks[Prompt.ask("Select an option", choices=[int(i) for i in range(1, len(self.tasks) + 1)]) - 1])
             case 3:
-                self.delete_task()
+                self.delete_task(self.tasks[Prompt.ask("Select an option", choices=[int(i) for i in range(1, len(self.tasks) + 1)]) - 1])
             case 4:
                 sort_by, filter_by = ui.list_options()
                 self.list_tasks(self.tasks, sort_by=sort_by, filter_by=filter_by)
@@ -38,21 +39,27 @@ class TaskManager:
 
     def create_task(self, task: Task):
         self.tasks.append(task)
+        self.save_to_db()
 
     def update_task(self, new_task: Task, old_task: Task):
         for (i, task) in enumerate(self.tasks):
             if task == old_task:
                 self.tasks[i] = new_task
+                self.save_to_db()
                 break
+        
 
     def delete_task(self, task_to_delete: Task):
-        pass
+        self.tasks.remove(task_to_delete)
+        self.save_to_db()
 
     def list_tasks(self, tasks: list[Task], sort_by=None, filter_by=None):
         return Task.compare(tasks, sort_by, filter_by) if sort_by or filter_by else tasks
 
-    def save_to_db(self, tasks: list[Task]):
-        pass
+    def save_to_db(self):
+        for task in self.tasks:
+            self.db.add_record('tasks', [task.description, task.complete, task.due_date, task.priority, task.create_date])
+        
 
     @staticmethod
     def exit():
