39f84e2b460ebbb605471d72fabb468e65ab38e7
MarksGB <131261270+22DP3MBruv@users.noreply.github.com>
2024-03-19 16:11:07 +0000

    Started to make the registration system with user class and user manager class. Added hashing and salting functions for passwords.

--- tasks/database_manager.py
+++ tasks/database_manager.py
@@ -1,5 +1,12 @@
 import sqlite3
 
+import re
+import uuid
+
+import hashlib
+import os
+import binascii
+
 class DatabaseManager:
     def __init__(self, db_name: str):
         self.db_name = db_name
@@ -31,3 +38,78 @@ class DatabaseManager:
 
     def close(self):
         self.db.close()
+
+class User:
+    def __init__(self, username, email, password):
+        self.user_id = self.generate_user_id()
+        self.username = self.validate_username(username)
+        self.email = self.validate_email(email)
+        self.password = self.validate_password(password)
+
+    @staticmethod
+    def generate_user_id():
+        return str(uuid.uuid4())
+
+    @staticmethod
+    def validate_username(username, db):
+        if not username:
+            raise ValueError("Username cannot be empty")
+        ## TODO: Add username_exists function to database
+        if db.username_exists(username):
+            raise ValueError("Username already exists")
+        return username
+
+    @staticmethod
+    def validate_email(email, db):
+        email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        if not re.fullmatch(email_regex, email):
+            raise ValueError("Invalid email address")
+        ## TODO: Add email_exists function to database
+        if db.email_exists(email):
+            raise ValueError("Email already exists")
+        return email
+
+    @staticmethod
+    def validate_password(password):
+        if len(password) < 8:
+            raise ValueError("Password must be at least 8 characters long")
+        return password
+    
+    @staticmethod
+    def hash_password(password):
+        
+        """Hash a password for storing."""
+        salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+        pwdhash = hashlib.pbkdf2_hmac('sha512', User.validate_password(password).encode('utf-8'), 
+                                    salt, 100000)
+        pwdhash = binascii.hexlify(pwdhash)
+        return (salt + pwdhash).decode('ascii')
+    
+    @staticmethod
+    def verify_password(stored_password, provided_password):
+        """Verify a stored password against one provided by user"""
+        salt = stored_password[:64]
+        stored_password = stored_password[64:]
+        pwdhash = hashlib.pbkdf2_hmac('sha512', 
+                                      provided_password.encode('utf-8'), 
+                                      salt.encode('ascii'), 
+                                      100000)
+        pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+        return pwdhash == stored_password
+
+
+
+
+class UserManager:
+    def __init__(self, db):
+        self.db = db
+
+    def create_user(self, username, email, password):
+        username = User.validate_username(username, self.db)
+        email = User.validate_email(email, self.db)
+        password = User.hash_password(password)
+        user_id = User.generate_user_id()
+
+        ## TODO: Add add_user function to database
+        self.db.add_user(user_id, username, email, password)
+        return User(user_id, username, email, password)
