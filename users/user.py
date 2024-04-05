8e80c28f3342dd250299874cc599fc8dc8dd1952
Robert <54269637+R-udren@users.noreply.github.com>
2024-04-05 06:20:20 +0000

    Changed password hashing salt length. IMPORTANT: All passwords now are invalid!!!

--- users/user.py
+++ users/user.py
@@ -1,7 +1,6 @@
 import re
-
 import hashlib
-import os
+import secrets
 import binascii
 
 
@@ -33,26 +32,27 @@ class User:
     def validate_password(password):
         if len(password) < 8:
             raise ValueError("Password must be at least 8 characters long")
+        if not re.match(r"^[A-Za-z0-9@#$%^&+=]*$", password):
+            raise ValueError("Password can only contain letters, numbers, and @#$%^&+=")
         return password
-    
+
     @staticmethod
     def hash_password(password):
-        
         """Hash a password for storing."""
-        salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
-        pwdhash = hashlib.pbkdf2_hmac('sha512', User.validate_password(password).encode('utf-8'),
-                                    salt, 100000)
-        pwdhash = binascii.hexlify(pwdhash)
-        return (salt + pwdhash).decode('ascii')
-    
+        salt = secrets.token_hex(16)
+        pwdhash = hashlib.pbkdf2_hmac('sha256', User.validate_password(password).encode('utf-8'),
+                                    salt.encode('ascii'), 100000)
+        pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+        return (salt + pwdhash)
+
     @staticmethod
     def verify_password(db_password, provided_password):
         """Verify a stored password against one provided by user"""
-        salt = db_password[:64]
-        stored_password = db_password[64:]
-        pwdhash = hashlib.pbkdf2_hmac('sha512', 
-                                      provided_password.encode('utf-8'), 
-                                      salt.encode('ascii'), 
+        salt = db_password[:32]
+        stored_password = db_password[32:]
+        pwdhash = hashlib.pbkdf2_hmac('sha256',
+                                      provided_password.encode('utf-8'),
+                                      salt.encode('ascii'),
                                       100000)
         pwdhash = binascii.hexlify(pwdhash).decode('ascii')
         return pwdhash == stored_password
