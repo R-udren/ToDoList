b166d7a00521f6ccafc64b02bd24d8a731fd06a9
Robert <54269637+R-udren@users.noreply.github.com>
2024-04-19 20:49:51 +0000

    Add helper.py module for date conversion

---
+++ tasks/helper.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from config import TIME_FORMAT
+
+def convert_to_datetime(date, default=None):
+    if date is None:
+        return default or datetime.now()
+    elif isinstance(date, datetime):
+        return date
+    elif isinstance(date, float):
+        return datetime.fromtimestamp(date)
+    elif isinstance(date, str):
+        return datetime.strptime(date, TIME_FORMAT)
+    else:
+        raise ValueError("Invalid date")
